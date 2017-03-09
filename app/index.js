cae6fd3fe59633e9983952da8b713956274be32d
daniel <danielemesh@gmail.com>
2017-03-09 18:01:48 +0200

    Combine Reducers - See description: 1. separated reducers 2. used combineReducers 3. added visibilityFilter reducer

--- app/index.js
+++ app/index.js
@@ -1,47 +1,83 @@
 import expect from "expect";
 import deepFreeze from "deep-freeze-strict";
-import { applyMiddleware, createStore } from "redux";
+
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import createLogger from "redux-logger";
 
 
 /* Reducers
-============================= */
-const todos = (state = [], action) => {
+ ============================= */
+const todo = (state, action) => {
     switch (action.type) {
         case "ADD_TODO":
-            return [
-                ...state,
-                {
-                    id:        action.id,
-                    text:      action.text,
-                    completed: false
-                }
-            ];
+            return {
+                id:        action.id,
+                text:      action.text,
+                completed: false
+            };
         case "TOGGLE_TODO":
-            return state.map(todo => {
-                if (todo.id !== action.id) {
-                    return todo;
-                }
-                return Object.assign({}, todo, { completed: !todo.completed });
+            if (state.id !== action.id) {
+                return state;
+            }
+            
+            return Object.assign({}, state, {
+                completed: !state.completed
             });
         default:
             return state;
     }
 };
 
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_TODO":
+            return [
+                ...state,
+                todo(undefined, action)
+            ];
+        case "TOGGLE_TODO":
+            return state.map(t => todo(t, action));
+        default:
+            return state;
+    }
+};
+
+const visibilityFilter = (state = "SHOW_ALL", action) => {
+  switch (action.type) {
+      case "SET_VISIBILITY_FILTER":
+          return action.filter;
+      default:
+          return state;
+  }
+};
+
+//
+//const todoApp = (state = {}, action) =>{
+//    return {
+//        todos: todos(state.todos, action),
+//        visibilityFilter: visibilityFilter(state.visibilityFilter, action)
+//    };
+//};
+
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+});
+
 const logger = createLogger();
-const store  = createStore(todos, applyMiddleware(logger));
+const store  = createStore(todoApp, applyMiddleware(logger));
 
 
 /* Dispatch actions
-============================= */
+ ============================= */
 store.dispatch({ type: "ADD_TODO", id: 0, text: "Learn Redux!" });
 store.dispatch({ type: "ADD_TODO", id: 1, text: "Go to the gym" });
 store.dispatch({ type: "TOGGLE_TODO", id: 1 });
+store.dispatch({ type: "SET_VISIBILITY_FILTER", filter: "SHOW_COMPLETED" });
 
 
 /* Tests
-============================= */
+ ============================= */
 const testAddTodo = () => {
     const stateBefore = [];
     const action      = {
