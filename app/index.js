460791c2b3365f44969e1a9603a32444ecb13ab0
daniel <danielemesh@gmail.com>
2017-03-12 16:46:38 +0200

    Removed some comments

--- app/index.js
+++ app/index.js
@@ -51,14 +51,6 @@ const visibilityFilter = (state = "SHOW_ALL", action) => {
   }
 };
 
-//
-//const todoApp = (state = {}, action) =>{
-//    return {
-//        todos: todos(state.todos, action),
-//        visibilityFilter: visibilityFilter(state.visibilityFilter, action)
-//    };
-//};
-
 const todoApp = combineReducers({
     todos,
     visibilityFilter
