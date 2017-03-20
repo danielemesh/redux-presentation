3b7dc5cdceef7f6d18565e8c95486b861222236e
daniel <danielemesh@gmail.com>
2017-03-20 14:47:19 +0200

    Demo - 34 - 36

--- app/index.js
+++ app/index.js
@@ -11,14 +11,17 @@ const store  = createStore(todoApp, applyMiddleware(logger));
 
 /* DOM elements
 ============================= */
+const submitBtn = $(".submit-btn");
 const todoField = $(".todo-field");
 const todoList = $(".todo-list");
 
 const render = () => {
     const todos = store.getState().todos;
     
-    let listItems = todos.map(todo => {
-        return `<li>${todo.text}</li>`;
+    const listItems = todos.map(todo => {
+        let completed  = todo.completed ? "completed" : "";
+        
+        return `<li id="${todo.id}" class="todo-item ${completed}">${todo.text}</li>`;
     });
     
     todoList.html(listItems);
@@ -27,19 +30,31 @@ const render = () => {
 store.subscribe(render);
 
 
-$(".submit-btn").click(event => {
+/* Event handlers
+============================= */
+submitBtn.click(event => {
     event.preventDefault();
     
     const todoText = todoField.val();
     
-    store.dispatch(addTodo(todoText));
+    if (todoText !== "") {
+        store.dispatch(addTodo(todoText));
     
-    todoField.val("");
+        todoField.val("");
+    }
+});
+
+todoList.click(".todo-item", event => {
+    const todoId = Number(event.target.id);
+    
+    if (!isNaN(todoId)) {
+        store.dispatch(toggleTodo(todoId));
+    }
 });
 
 /* Dispatch actions
  ============================= */
-//store.dispatch(addTodo("Learn Redux"));
-//store.dispatch(addTodo("Do something!"));
+store.dispatch(addTodo("Learn Redux"));
+store.dispatch(addTodo("Do something!"));
 //store.dispatch(toggleTodo(1));
 //store.dispatch(setVisibilityFilter(SHOW_ACTIVE));
